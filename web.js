00359d7e6c4e2186c4ecfbcc0d76d7f413a8b4b7
George Mathew <george.matew@gmail.com>
2013-07-21 12:41:55 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+var holder=new Buffer(128);
+holder=fs.readFileSync('index.html','utf-8');
+  response.send(holder.toString("utf-8",0,(holder.length)));
 });
 
 var port = process.env.PORT || 5000;
